4774bdcaa08202dedb18f7842a4626696bdca18b
Rajat Rajput <rajputrajat@gmail.com>
2021-05-29 14:24:32 +0530

    staticlib working fine. but double quotes are also being printed. which shouldn't happen

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
+use std::ffi::CStr;
 use std::os::raw::c_char;
 
 #[no_mangle]
 pub extern "C" fn set_and_get(in_str: *const c_char, in_num: u16) -> *const c_char {
-    println!(
-        "This is from C \"{:?}\". this one too: {:?}",
-        in_str, in_num
-    );
+    let in_str = unsafe { CStr::from_ptr(in_str) };
+    println!("This is from C {:?}. this one too: {:?}", in_str, in_num);
     b"Hi from Rust!".as_ptr().cast()
 }
